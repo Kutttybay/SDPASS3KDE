52b2cef14e998726e97923f755c3cb7d31638076
kutttybay <kutttybay@gmail.com>
2023-10-22 22:10:47 +0600

    2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello, this assignment 3 by Kuttybaev Daulet");
     }
 }
